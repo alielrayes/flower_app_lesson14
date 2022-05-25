721adf9dfea18565625ee0afb6a22a2b2c9d0008
alielrayes <61753691+alielrayes@users.noreply.github.com>
2022-05-25 15:21:30 +0200

    first commit

---
+++ lib/pages/login.dart
@@ -0,0 +1,122 @@
+// ignore_for_file: prefer_const_literals_to_create_immutables, prefer_const_constructors
+
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flower_app/pages/register.dart';
+import 'package:flower_app/shared/colors.dart';
+import 'package:flower_app/shared/contants.dart';
+import 'package:flower_app/shared/snackbar.dart';
+import 'package:flutter/material.dart';
+
+class Login extends StatefulWidget {
+  Login({Key? key}) : super(key: key);
+
+  @override
+  State<Login> createState() => _LoginState();
+}
+
+class _LoginState extends State<Login> {
+  final emailController = TextEditingController();
+
+  final passwordController = TextEditingController();
+
+  signIn() async {
+    try {
+      final credential = await FirebaseAuth.instance.signInWithEmailAndPassword(
+          email: emailController.text, password: passwordController.text);
+          showSnackBar(context, "doneee");
+    } on FirebaseAuthException catch (e) {
+      if (e.code == 'user-not-found') {
+        showSnackBar(context, "No user found for that email.");
+      } else if (e.code == 'wrong-password') {
+        showSnackBar(context, "Wrong password provided for that user.");
+      }
+    }
+  }
+
+  @override
+  void dispose() {
+    // TODO: implement dispose
+    emailController.dispose();
+    passwordController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Scaffold(
+        backgroundColor: Color.fromARGB(255, 247, 247, 247),
+        body: Center(
+          child: Padding(
+            padding: const EdgeInsets.all(33.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                const SizedBox(
+                  height: 64,
+                ),
+                TextField(
+                    controller: emailController,
+                    keyboardType: TextInputType.emailAddress,
+                    obscureText: false,
+                    decoration: decorationTextfield.copyWith(
+                      hintText: "Enter Your Email : ",
+                    )),
+                const SizedBox(
+                  height: 33,
+                ),
+                TextField(
+                    controller: passwordController,
+                    keyboardType: TextInputType.text,
+                    obscureText: true,
+                    decoration: decorationTextfield.copyWith(
+                      hintText: "Enter Your Password : ",
+                    )),
+                const SizedBox(
+                  height: 33,
+                ),
+                ElevatedButton(
+                  onPressed: () async {
+                    await signIn();
+                    if (!mounted) return;
+                    // showSnackBar(context, "Done ... ");
+                  },
+                  style: ButtonStyle(
+                    backgroundColor: MaterialStateProperty.all(BTNgreen),
+                    padding: MaterialStateProperty.all(EdgeInsets.all(12)),
+                    shape: MaterialStateProperty.all(RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(8))),
+                  ),
+                  child: Text(
+                    "Sign in",
+                    style: TextStyle(fontSize: 19),
+                  ),
+                ),
+                const SizedBox(
+                  height: 33,
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text("Do not have an account?",
+                        style: TextStyle(fontSize: 18)),
+                    TextButton(
+                        onPressed: () {
+                          Navigator.pushReplacement(
+                            context,
+                            MaterialPageRoute(builder: (context) => Register()),
+                          );
+                        },
+                        child: Text('sign up',
+                            style:
+                                TextStyle(color: Colors.black, fontSize: 18))),
+                  ],
+                )
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
